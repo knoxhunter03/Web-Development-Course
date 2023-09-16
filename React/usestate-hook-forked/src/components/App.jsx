5ebe62f86ccd6b464ead88721e7bca1f4dad7d42
Mangalam K <mangalamkallo03@gmail.com>
2023-09-17 02:21:18 +0530

    Learned REACT Hooks

---
+++ React/usestate-hook-forked/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App() {
+  // useState to be called inside a function
+  const [count, setCount] = React.useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
