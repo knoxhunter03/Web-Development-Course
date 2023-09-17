cdae10ab844ff29d85ae1e3ef16e228ef6d0fd3b
Mangalam K <mangalamkallo03@gmail.com>
2023-09-17 19:46:43 +0530

    Add Complex State Handling

---
+++ React/changing-complex-state-forked/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleOnChange(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fname: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          onChange={handleOnChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleOnChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
