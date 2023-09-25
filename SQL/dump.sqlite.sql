c6ee958fff90a3dd0bcc0fe87e896aeb3c4a6a57
Mangalam K <mangalamkallo03@gmail.com>
2023-09-25 20:29:14 +0530

    Revised SQL Basic Commands

---
+++ SQL/dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE products (
+	id INT NOT NULL,
+    name STRING,
+    price MONEY, stocks INT,
+    PRIMARY KEY (id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
