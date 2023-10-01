41baf33addc80dfec43ca9b48e96a42007268b44
Mangalam K <mangalamkallo03@gmail.com>
2023-10-01 07:24:20 +0530

    Create MERN Authentication to WorkoutBuddy

--- My Projects/WorkoutBuddy (MERN)/backend/server.js
+++ My Projects/WorkoutBuddy (MERN)/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import 'dotenv/config';
 import workoutRoutes from "./routes/workouts.js";
+import userRoutes from "./routes/user.js";
 import mongoose from "mongoose";
 
 //express app
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
