3f9db5806e4d8de10b529675c76d9f27c97c4b15
Mangalam K <mangalamkallo03@gmail.com>
2023-10-01 08:49:45 +0530

    Add LogOut Button

--- My Projects/WorkoutBuddy (MERN)/frontend/src/pages/Signup.js
+++ My Projects/WorkoutBuddy (MERN)/frontend/src/pages/Signup.js
@@ -1,13 +1,15 @@
 import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
 
 const Signup = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await signup(email, password)
     }
 
     return (
@@ -24,11 +26,12 @@ const Signup = () => {
             <label>Password:</label>
             <input 
                 type="password"
-                onChange={(e) => setPassowrd(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
 
-            <button>Sign Up</button>
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className="error">{error}</div>}
 
         </form>
     )
