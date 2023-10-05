f91984de6203e4f788130062f94b4419c3001501
Mangalam K <mangalamkallo03@gmail.com>
2023-10-05 21:55:02 +0530

    Learn about useReducer

--- React/react-context-hooks/src/App.js
+++ React/react-context-hooks/src/App.js
@@ -1,10 +1,11 @@
 import ContextTutorial from "./components/ContextTutorial";
+import LearnReducer from "./components/useReducer/LearnReducer";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <ContextTutorial />
+      <LearnReducer />
     </div>
   );
 }
---
+++ React/react-context-hooks/src/components/useReducer/LearnReducer.js
@@ -0,0 +1,50 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "INCREMENT":
+            return {
+                count: state.count + 1,
+                showtext: state.showtext
+            };
+        
+        case "toggleShowText":
+            return {
+                count: state.count,
+                showtext: !state.showtext
+            };
+        
+        default:
+            return state
+    };
+}
+
+
+const LearnReducer = () => {
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: 0,
+        showtext: true
+    });
+
+    return(
+        <div>
+            <h1>{state.count}</h1>
+            <button
+                onClick={() => {
+                    dispatch({type: "INCREMENT"});
+                    dispatch({type: "toggleShowText"});
+                }}
+            >
+                Click Here
+            </button>
+            {state.showtext && <p>This is a text</p>}
+        </div>
+    )
+}
+
+
+
+
+
+export default LearnReducer;
