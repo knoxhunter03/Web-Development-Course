67a8acb5c22437cfe34caf87e02b3564d7a4de66
Mangalam K <mangalamkallo03@gmail.com>
2023-10-09 20:07:52 +0530

    Add useLayoutEffect

--- React/react-context-hooks/src/App.js
+++ React/react-context-hooks/src/App.js
@@ -2,12 +2,13 @@ import ContextTutorial from "./components/ContextTutorial";
 import LearnReducer from "./components/useReducer/LearnReducer";
 import LearnEffect from "./components/useEffect/LearnEffect";
 import LearnRef from "./components/useRef/LearnRef";
+import LearnLayoutEffect from "./components/useLayoutEffect/LearnLayoutEffect";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <LearnRef />
+      <LearnLayoutEffect />
     </div>
   );
 }
